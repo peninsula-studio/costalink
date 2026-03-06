a72345a1d5e7420df2f88e79b10ac3e6351213e5
jorgebef <jorge.befan@gmail.com>
2026-03-06 20:57:55 +0100

    manage auth middlewares

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import { redirect } from "@tanstack/react-router";
 import { createMiddleware } from "@tanstack/react-start";
 import { getSessionCookie } from "better-auth/cookies";
+import { getSessionFn } from "@/lib/fn/auth";
 
 export const sessionCookieMiddleware = createMiddleware().server(
   async ({ next, request }) => {
@@ -15,15 +16,22 @@ export const sessionCookieMiddleware = createMiddleware().server(
   },
 );
 
-// export const adminMiddleware = createMiddleware({ type: "function" })
-//   // .inputValidator(
-//   //   (data: { context: { session: typeof auth.$Infer.Session } }) => data,
-//   // )
-//   .server(async ({ next, context }) => {
-//     console.log("Checking if user is Admin...");
-//     const session = await getSessionFn();
-//     if (context?.session.user.role !== "admin") {
-//       throw redirect({ to: "/dashboard" });
-//     }
-//     return await next({ context: { session } });
-//   });
+export const authMiddleware = createMiddleware({ type: "function" }).server(
+  async ({ next }) => {
+    const session = await getSessionFn();
+    if (!session) {
+      throw redirect({ to: "/sign-in" });
+    }
+    return await next({ context: { session } });
+  },
+);
+
+export const adminMiddleware = createMiddleware({ type: "function" })
+  .middleware([authMiddleware])
+  .server(async ({ next, context }) => {
+    console.log("Checking if user is Admin...");
+    if (context.session.user.role !== "admin") {
+      throw redirect({ to: "/app" });
+    }
+    return await next({ context });
+  });
